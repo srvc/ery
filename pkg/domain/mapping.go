3029c81dd6aefd937b95ef58f409d4b1cca3f668
izumin5210 <m@izum.in>
2018-06-09 18:49:54 +0900

    Refine domain

--- pkg/domain/mapping.go
+++ pkg/domain/mapping.go
@@ -1,7 +1,15 @@
 package domain
 
-// Mapping represents <hostname> - <local IP>:<port> map.
+// PortAddrMap is mapping of ports and addresses.
+type PortAddrMap map[Port]Addr
+
+// Mapping represents <hostname>:<port> - <local IP>:<port> map.
 type Mapping struct {
-	Addr
-	Hostnames []string
+	Host        string      `json:"host"`
+	PortAddrMap PortAddrMap `json:"map"`
+}
+
+// Map returns an Addr mapped on the given port.
+func (m *Mapping) Map(port Port) Addr {
+	return m.PortAddrMap[port]
 }
