87f1fe942d596c1a417f9984e3da7b1ee9fa773a
izumin5210 <m@izum.in>
2018-06-11 02:56:21 +0900

    Add ListenEvent() to MappingRepository

--- pkg/data/local/mapping_repository.go
+++ pkg/data/local/mapping_repository.go
@@ -3,6 +3,7 @@ package local
 import (
 	"context"
 	"sync"
+	"sync/atomic"
 
 	"github.com/pkg/errors"
 
@@ -13,11 +14,14 @@ import (
 func NewMappingRepository() domain.MappingRepository {
 	return &mappingRepositoryImpl{
 		mappingByHost: new(sync.Map),
+		eventEmitters: new(sync.Map),
 	}
 }
 
 type mappingRepositoryImpl struct {
-	mappingByHost *sync.Map
+	mappingByHost     *sync.Map
+	eventEmitters     *sync.Map
+	eventEmitterIDSeq uint64
 }
 
 func (r *mappingRepositoryImpl) List(ctx context.Context) ([]*domain.Mapping, error) {
@@ -56,10 +60,70 @@ func (r *mappingRepositoryImpl) Create(ctx context.Context, m *domain.Mapping) e
 
 	r.mappingByHost.Store(m.Host, m)
 
+	r.emitEvent(domain.MappingEvent{
+		Type:    domain.MappingEventCreated,
+		Mapping: *m,
+	})
+
 	return nil
 }
 
 func (r *mappingRepositoryImpl) DeleteByHost(ctx context.Context, host string) error {
-	r.mappingByHost.Delete(host)
+	if v, ok := r.mappingByHost.Load(host); ok {
+		r.mappingByHost.Delete(host)
+		if m, ok := v.(*domain.Mapping); ok {
+			r.emitEvent(domain.MappingEvent{
+				Type:    domain.MappingEventDestroyed,
+				Mapping: *m,
+			})
+		}
+	}
 	return nil
+}
+
+func (r *mappingRepositoryImpl) ListenEvent(ctx context.Context) (<-chan domain.MappingEvent, <-chan error) {
+	evCh := make(chan domain.MappingEvent)
+	errCh := make(chan error, 1)
+
+	id := atomic.AddUint64(&r.eventEmitterIDSeq, 1)
+	r.eventEmitters.Store(id, &mappingEventEmitter{
+		id:    id,
+		evCh:  evCh,
+		errCh: errCh,
+		ctx:   ctx,
+	})
+
+	return evCh, errCh
+}
+
+func (r *mappingRepositoryImpl) emitEvent(ev domain.MappingEvent) {
+	var disposableIDs []uint64
+	r.eventEmitters.Range(func(_, v interface{}) bool {
+		if emitter, ok := v.(*mappingEventEmitter); !ok || !emitter.Emit(ev) {
+			disposableIDs = append(disposableIDs, emitter.id)
+		}
+		return true
+	})
+
+	for _, id := range disposableIDs {
+		r.eventEmitters.Delete(id)
+	}
+}
+
+type mappingEventEmitter struct {
+	id    uint64
+	evCh  chan<- domain.MappingEvent
+	errCh chan<- error
+	ctx   context.Context
+}
+
+func (e *mappingEventEmitter) Emit(ev domain.MappingEvent) bool {
+	select {
+	case <-e.ctx.Done():
+		e.errCh <- e.ctx.Err()
+		return false
+	default:
+		e.evCh <- ev
+		return true
+	}
 }
