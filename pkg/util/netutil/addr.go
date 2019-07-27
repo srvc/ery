e579817cfb98bfede35ea4603651430a1976358a
izumin5210 <m@izum.in>
2019-07-27 18:47:01 +0900

    ery Next

--- pkg/util/netutil/addr.go
+++
@@ -1,8 +0,0 @@
-package netutil
-
-import "strconv"
-
-// HostAndPort joins a hostname and a port.
-func HostAndPort(host string, port uint32) string {
-	return host + ":" + strconv.FormatUint(uint64(port), 10)
-}
